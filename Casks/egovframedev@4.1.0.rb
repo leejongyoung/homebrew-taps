fd4959dd9ebd666b08ad61007cd53fa8dd74cd78
Lee Jongyoung <leejongyoung@icloud.com>
2023-12-24 11:33:25 +0900

    Add new .icns mac icon for version 4.1 and 4.2

--- Casks/egovframedev@4.1.0.rb
+++ Casks/egovframedev@4.1.0.rb
@@ -12,9 +12,18 @@ cask "egovframedev@4.1.0" do
 
   app "eGovFrameDev-#{version}-Mac-#{arch}.app"
 
+  postflight do
+    icon_path = "#{Caskroom.path}/egovframedev@4.1.0/#{version}/app.icns"
+    app_path = "/Applications/eGovFrameDev-#{version}-Mac-#{arch}.app"
+    system_command "mkdir", args: ["-p", File.dirname(icon_path)]
+    system_command "curl", args: ["-s", "-o", icon_path, "https://raw.githubusercontent.com/egovframework/homebrew-egovframe/main/app.icns"]
+    system_command "xattr", args: ["-d", "com.apple.quarantine", app_path]
+    system_command "cp", args: [icon_path, "#{app_path}/Contents/Resources/Eclipse.icns"]
+    system_command "touch", args: [app_path]
+  end
+
   zap trash: [
     "~/Library/Preferences/org.eclipse.platform.ide.plist",
     "~/Library/Saved Application State/org.eclipse.platform.ide.savedState",
   ]
 end
-
